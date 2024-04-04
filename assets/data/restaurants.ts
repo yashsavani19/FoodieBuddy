344519313424cd8a89f816bbc01787c14032a0e0
FranTheCaveman <franciscah.nel3@gmail.com>
2024-04-04 21:37:30 +1300

    Created 10 hardcoded restaurant data for testing

---
+++ assets/data/restaurants.ts
@@ -0,0 +1,75 @@
+const restaurants = [
+    {
+      id: 1,
+      name: 'Ikkyu Japanese Restaurant',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipOycLuzZSL_ZXFkc3VVEAs_9AUfe1Btt9hhL8Xs=w408-h345-k-no',
+      distance: 5,
+    },
+    {
+      id: 2,
+      name: 'Sensational Chicken Mount St',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipMR-rQ2Ghjs0kQusZH3MCcorrRvbosQOAn5GRy-=w408-h306-k-no',
+      distance: 5,
+    },
+    {
+      id: 3,
+      name: 'Mali Thai',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipOaXXpoTgTubxRQ_ljldRfze3DtyfWGFmbFyOnN=w412-h240-k-no',
+      distance: 5,
+    },
+    {
+      id: 4,
+      name: "Chawla's Indian Restaurant",
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipNuOTwULnxHIbcV_92M2Xk091XPOHamP-M112bh=w408-h306-k-no',
+      distance: 9.9,
+    },
+    {
+      id: 5,
+      name: "McDonald's Queen Street",
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipMOVtUeE-Vby3RbbkUaRO33yG0Du4nrQsqf_8GE=w408-h306-k-no',
+      distance: 12,
+    },
+    {
+      id: 6,
+      name: "Nando's Queen St",
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipNTTZzynDcdp2VTLMou8qdQhNELPFpZkc8pN1OV=w408-h306-k-no',
+      distance: 10.5,
+    },
+    {
+      id: 7,
+      name: 'BurgerFuel Queen Street',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipN1kuECt1X67EnyHbsG1ORkNFJEKvgkOo66I2dp=w408-h306-k-no',
+      distance: 16,
+    },
+    {
+      id: 8,
+      name: 'Tandoor Indian Restaurant',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipMHckIcfcOEqnzKl_qztSvBySNw5WXZk3uWopH_=w450-h240-k-no',
+      distance: 12,
+    },
+    {
+      id: 9,
+      name: "Celine Mediterranean Cuisine",
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipOjB3VU51yaB3ByhXvl0XjzEby5bpN8jbivbxwD=w408-h544-k-no',
+      distance: 13,
+    },
+    {
+      id: 10,
+      name: 'Boracay Garden Restaurant Ltd',
+      image:
+        'https://lh5.googleusercontent.com/p/AF1QipPHnkcpOlQJlQUaSTaLs0iYVs2K5Ou5aegkuc1Y=w408-h306-k-no',
+      distance: 13,
+    },
+  ];
+  
+  export default restaurants;
+  
