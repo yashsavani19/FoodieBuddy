c9c25eedf01e16bd420a7701360c86a9f1c7c12a
FranTheCaveman <franciscah.nel3@gmail.com>
2024-04-05 02:01:49 +1300

    Added external links to images (temp for testing)

---
+++ assets/data/images.ts
@@ -0,0 +1,7 @@
+export default {
+    defaultRestaurantImage: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihaCmRyzYpIf2asmaW_xsW78AdFGkvgk3tD4AzY6Bl-OJbu9ivs2MbG29PrGGbMuaq8EguckW7fyEaBwaQK-J6h8KXfeCe15RwM=w1920-h838-v0",
+    mapMarker: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihYmVoQ_5bpc90aT35Jtrtm-pN7RtE3fby6w0-A14yhuoj6vMipb0KtQvzOtSPWZTrTVB1Fqk6OeXbm7hcVXgp1qNj4LC840Gdw=w1920-h838-v0",
+    faveIcon: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihZ8JV5Y6SqVD4-Ogx7vtX5xM0VjgO3iomJ4dzHxJmvlYB22McUfRZMCupV5PzfU_5PnD1FwamV8Rf3E8gLMbHvrn036ZIaLRA=w1920-h838-v0",
+    bookmarkIcon: "https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihYpXUQLdrrLYJpcjHHfFjifzzUTp-SIq_fK24KTYpoR5ri9vDdwTigce4SoKXr390stImRmamBrSRTEfHJlb6ArKb5qIsk4CCA=w1920-h838-v0",
+};
+  
