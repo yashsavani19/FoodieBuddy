7c91c4141ebb66ce5971328666506f78c1890034
Lucas Hashemi <160118560+itsLucas-h@users.noreply.github.com>
2024-04-12 20:31:03 +1200

    New Icons

--- app/(tabs)/Map.tsx
+++ app/(tabs)/Map.tsx
@@ -1,32 +1,37 @@
+
 import EditScreenInfo from "@/components/EditScreenInfo";
 //import {View} from '@/components/Themed';
-
-import { PermissionsAndroid, TouchableOpacity } from "react-native";
-import MapView, { LatLng, Marker, PROVIDER_GOOGLE } from "react-native-maps";
-import { Dimensions, StyleSheet, View, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import MapView, { Callout, LatLng, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import {
   GooglePlaceDetail,
   GooglePlacesAutocomplete,
 } from "react-native-google-places-autocomplete";
 import { GOOGLE_API_KEY } from "@env";
 import Constants from "expo-constants";
-import { useEffect, useRef, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import MapViewDirections from "react-native-maps-directions";
 import * as Location from "expo-location";
 import axios from "axios";
-
-const { width, height } = Dimensions.get("window");
-
-const ASPECT_RATIO = width / height;
-// const LATITUDE_DELTA = 0.02;
-// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+// Initial position for the map
 const INITIAL_POSITION = {
   latitude: -36.8485,
   longitude: 174.7633,
   latitudeDelta: 0.09,
   longitudeDelta: 0.04,
 };
-
+// Interface for defining props of the RestaurantMarker component
+interface RestaurantMarkerProps {
+  rating: number | string;
+}
+// Interface for defining structure of Restaurant object
 interface Restaurant {
   geometry: {
     location: {
@@ -34,15 +39,22 @@ interface Restaurant {
       lng: number;
     };
   };
+  place_id: string;
   name: string;
+  rating?: number;
+  details?: {
+    rating: string;
+    phoneNumber?: string;
+    photoUrl?: string | null;
+  };
 }
-
+// Interface for defining props of InputAutoComplete component
 type InputAutoCompleteProps = {
   label: string;
   placeholder: string;
   onPlaceSelected: (details: GooglePlaceDetail | null) => void;
 };
-
+// InputAutoComplete component for handling autocomplete input fields
 function InputAutoComplete({
   label,
   placeholder,
@@ -66,8 +78,9 @@ function InputAutoComplete({
     </>
   );
 }
-
+// Map component
 export default function Map() {
+  // State variables
   const [origin, setOrigin] = useState<LatLng | null>();
   const [destination, setDestination] = useState<LatLng | null>();
   const [currentLocation, setCurrentLocation] = useState<LatLng | null>();
@@ -75,8 +88,13 @@ export default function Map() {
   const [distance, setDistance] = useState(0);
   const [duration, setDuration] = useState(0);
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+  
+  
+  // Ref for the MapView component
   const mapRef = useRef<MapView>(null);
-
+  
+  // useEffect hook for fetching current location and nearby restaurants on component mount
   useEffect(() => {
     async function fetchCurrentLocation() {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -84,7 +102,6 @@ export default function Map() {
         console.error("Permission to access location was denied");
         return;
       }
-
       let location = await Location.getCurrentPositionAsync({});
       setCurrentLocation({
         latitude: location.coords.latitude,
@@ -94,37 +111,14 @@ export default function Map() {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
       });
-
-      // Function to re-center map on the current location
-      const centerOnUser = () => {
-        if (currentLocation) {
-          mapRef.current?.animateCamera(
-            {
-              center: {
-                latitude: currentLocation.latitude,
-                longitude: currentLocation.longitude,
-              },
-              pitch: 0,
-              heading: 0,
-              altitude: 0,
-              zoom: 15, // You can adjust the zoom level as needed
-            },
-            { duration: 1000 }
-          ); // Animation duration in ms
-        } else {
-          console.error("Current location is not available");
-        }
-      };
-
       fetchNearbyRestaurants(
         location.coords.latitude,
         location.coords.longitude
       );
     }
-
     fetchCurrentLocation();
   }, []);
-
+  // Function to fetch nearby restaurants
   const fetchNearbyRestaurants = async (
     latitude: number,
     longitude: number
@@ -137,7 +131,35 @@ export default function Map() {
       console.error("Failed to fetch restaurants:", error);
     }
   };
+  const fetchRestaurantDetails = async (placeId: string, index: number) => {
+  const detailsUrl = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,rating,formatted_phone_number,photos&key=${GOOGLE_API_KEY}`;
+  try {
+    const response = await axios.get(detailsUrl);
+    const result = response.data.result;
+    const photoUrl = result.photos && result.photos.length > 0 ? getPhotoUrl(result.photos[0].photo_reference) : null;
+    
+    const updatedRestaurants = [...restaurants]; // Copy the current state
+    updatedRestaurants[index].details = { // Update only the specific restaurant's details
+      rating: result.rating,
+      phoneNumber: result.formatted_phone_number,
+      photoUrl: photoUrl
+    };
+    setRestaurants(updatedRestaurants); // Set the updated array back to the state
+  } catch (error) {
+    console.error("Failed to fetch restaurant details:", error);
+  }
+};
 
+const handleMarkerPress = (placeId: string, index: number) => {
+  fetchRestaurantDetails(placeId, index);
+};
+
+  const getPhotoUrl = (photoReference: string) => {
+    return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoReference}&key=${GOOGLE_API_KEY}`;
+  };
+  
+  
+  // Function to move the map to a specific position
   const moveTo = async (position: LatLng) => {
     const camera = await mapRef.current?.getCamera();
     if (camera) {
@@ -145,38 +167,21 @@ export default function Map() {
       mapRef.current?.animateCamera(camera, { duration: 1000 });
     }
   };
-
-  const changeZoom = (factor: number) => {
-    mapRef.current?.getCamera().then((camera) => {
-      if (camera && camera.zoom !== undefined) {
-        camera.zoom += factor;
-        mapRef.current?.animateCamera(camera, { duration: 1000 });
-      }
-    });
-  };
-
-  const edgePaddingValue = 150;
-  const edgePadding = {
-    top: edgePaddingValue,
-    right: edgePaddingValue,
-    bottom: edgePaddingValue,
-    left: edgePaddingValue,
-  };
-
+  // Function called when route trace is ready
   const traceRouteOnReady = (args: any) => {
     if (args) {
       setDistance(args.distance);
       setDuration(args.duration);
     }
   };
-
+  // Function to trace the route between origin and destination
   const traceRoute = () => {
     if (origin && destination) {
       setShowDirections(true);
-      mapRef.current?.fitToCoordinates([origin, destination], { edgePadding });
+      mapRef.current?.fitToCoordinates([origin, destination]);
     }
   };
-
+  // Function called when a place is selected from autocomplete
   const onPlaceSelected = (
     details: GooglePlaceDetail | null,
     flag: "origin" | "destination"
@@ -189,6 +194,7 @@ export default function Map() {
     set(position);
     moveTo(position);
   };
+  // Rendering the map component
   return (
     <View style={styles.container}>
       <MapView
@@ -197,21 +203,47 @@ export default function Map() {
         provider={PROVIDER_GOOGLE}
         initialRegion={INITIAL_POSITION}
       >
+        {/* Current location marker */}
         {currentLocation && (
-          <Marker coordinate={currentLocation} title="You are here" />
+          <Marker coordinate={currentLocation} title="You are here">
+            <View style={styles.customMarker}>
+              <MaterialCommunityIcons
+                name="map-marker-account"
+                size={60}
+                color="black"
+              />
+            </View>
+          </Marker>
         )}
+        
+        {/* Restaurants markers */}
         {restaurants.map((restaurant, index) => (
-          <Marker
-            key={index}
-            coordinate={{
-              latitude: restaurant.geometry.location.lat,
-              longitude: restaurant.geometry.location.lng,
-            }}
-            title={restaurant.name}
-          />
-        ))}
+  <Marker
+    key={index}
+    coordinate={{
+      latitude: restaurant.geometry.location.lat,
+      longitude: restaurant.geometry.location.lng,
+    }}
+    title={restaurant.name}
+    onPress={() => handleMarkerPress(restaurant.place_id, index)}
+  >
+    <RestaurantMarker rating={restaurant.rating ? restaurant.rating.toString() : 'N/A'} />
+    <Callout tooltip style={styles.customCallout}>
+      <View style={styles.calloutContainer}>
+        <Text style={styles.calloutTitle}>{restaurant.name}</Text>
+        {/* Additional information can be dynamically added here based on state updated by handleMarkerPress */}
+        <Text style={styles.calloutText}>Rating: {restaurant.rating || 'N/A'}</Text>
+        {/* You can conditionally render an Image component if a photo is available in the state */}
+      </View>
+    </Callout>
+  </Marker>
+))}
+        
+        {/* Origin and destination markers */}
         {origin && <Marker coordinate={origin} />}
         {destination && <Marker coordinate={destination} />}
+        
+        {/* Directions */}
         {showDirections && origin && destination && (
           <MapViewDirections
             origin={origin}
@@ -223,6 +255,8 @@ export default function Map() {
           />
         )}
       </MapView>
+      
+      {/* Search container */}
       <View style={styles.searchContainer}>
         <InputAutoComplete
           label="Origin"
@@ -241,37 +275,6 @@ export default function Map() {
         <TouchableOpacity style={styles.button} onPress={traceRoute}>
           <Text style={styles.buttonText}>Trace route</Text>
         </TouchableOpacity>
-        <View style={styles.zoomButtons}>
-          <TouchableOpacity
-            style={styles.zoomButton}
-            onPress={() => changeZoom(1)}
-          >
-            <Text style={styles.buttonText}>+</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.zoomButton}
-            onPress={() => changeZoom(-1)}
-          >
-            <Text style={styles.buttonText}>-</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.centerButton}
-            onPress={() => {
-              if (currentLocation) {
-                mapRef.current?.animateToRegion(
-                  {
-                    ...currentLocation,
-                    latitudeDelta: INITIAL_POSITION.latitudeDelta,
-                    longitudeDelta: INITIAL_POSITION.longitudeDelta,
-                  },
-                  1000
-                ); 
-              }
-            }}
-          >
-            <Text style={styles.buttonText}>Center on Me</Text>
-          </TouchableOpacity>
-        </View>
         {distance && duration ? (
           <View>
             <Text>Distance: {distance.toFixed(2)}</Text>
@@ -279,10 +282,45 @@ export default function Map() {
           </View>
         ) : null}
       </View>
+      
+      {/* Center button */}
+      <View>
+        <TouchableOpacity
+          style={styles.centerButton}
+          onPress={() => {
+            if (currentLocation) {
+              mapRef.current?.animateToRegion(
+                {
+                  ...currentLocation,
+                  latitudeDelta: INITIAL_POSITION.latitudeDelta,
+                  longitudeDelta: INITIAL_POSITION.longitudeDelta,
+                },
+                1000
+              );
+            }
+          }}
+        >
+          <MaterialCommunityIcons name="crosshairs" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
+// RestaurantMarker component
+const RestaurantMarker: FC<RestaurantMarkerProps> = ({ rating }) => {
+  return (
+    <View style={styles.markerContainer}>
+      <View style={styles.bubble}>
+        <View style={styles.iconCircle}>
+          <MaterialCommunityIcons name="silverware-fork-knife" size={16} color="white" />
+        </View>
+        <Text style={styles.rating}>{rating}</Text>
+      </View>
+      <View style={styles.pointer} />
+    </View>
+  );
+};
+// Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -320,19 +358,80 @@ const styles = StyleSheet.create({
   buttonText: {
     textAlign: "center",
   },
-  zoomButtons: {
-    flexDirection: "row",
-    marginTop: 10,
-  },
-  zoomButton: {
-    backgroundColor: "#bbb",
-    padding: 10,
-    marginHorizontal: 5,
-    borderRadius: 4,
-  },
   centerButton: {
-    backgroundColor: "#bbb",
+    position: "absolute",
+    backgroundColor: "#ffffff", 
+    height: 45,
+    width: 45,
+    borderRadius: 22.5, 
+    bottom: 30,
+    left: 140, 
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000", 
+    shadowOffset: { width: 0, height: 2 }, 
+    shadowOpacity: 0.25, 
+    shadowRadius: 3.84, 
+    elevation: 5, 
+  },
+  markerContainer: {
+    alignItems: 'center',
+  },
+  bubble: {
+    flexDirection: "row",
+    backgroundColor: "rgba(100, 100, 100, 0.7)", 
+    paddingHorizontal: 6,
+    paddingVertical: 3,
+    borderRadius: 20,
+    alignItems: "center",
+  },
+  iconCircle: {
+    backgroundColor: "black", 
+    borderRadius: 10,
+    width: 20,
+    height: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 5,
+  },
+  rating: {
+    color: "white",
+    fontSize: 14,
+  },
+  pointer: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderTopWidth: 6,
+    borderRightWidth: 6,
+    borderBottomWidth: 0,
+    borderLeftWidth: 6,
+    borderTopColor: "rgba(100, 100, 100, 0.7)", 
+    borderRightColor: 'transparent',
+    borderBottomColor: 'transparent',
+    borderLeftColor: 'transparent',
+    marginTop: -1,
+  },
+  customMarker: {
+  },
+  customCallout: {
+    borderRadius: 6,
     padding: 10,
-    borderRadius: 4,
+    backgroundColor: 'white',
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.3,
+  },
+  calloutContainer: {
+    width: 180, // Adjust size as needed
+  },
+  calloutTitle: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  calloutText: {
+    fontSize: 12,
   },
 });
