306ea0111f1c0885fb26a21d7022de6c07122aa3
Joshua-Frost <gky3846@autuni.ac.nz>
2024-05-03 20:50:50 +1200

    Cleaned up database handler

--- controller/DatabaseHandler.tsx
+++ controller/DatabaseHandler.tsx
@@ -285,7 +285,7 @@ export const addUser = async (uid: string, email: string, username: string) => {
 };
 
 /**
- * Fetches user data
+ * Fetches user data from database
  */
 export const fetchUser = async (uid: string) => {
   try {
@@ -303,96 +303,3 @@ export const fetchUser = async (uid: string) => {
     alert("Internal error fetching user. Please try again later.");
   }
 };
-
-// /**
-//  * Updates username
-//  * @param newUsername new username
-//  */
-// export const updateUsername = async (uid: string, newUsername: string) => {
-//   try {
-//     await setDoc(
-//       doc(db, userCollection),
-//       { username: newUsername },
-//       { merge: true }
-//     );
-//   } catch (e) {
-//     console.error("Error updating document: ", e);
-//     alert("Internal error updating username. Please try again later.");
-//   }
-// };
-
-// // WARNING: Currently only updates database email, not authentication email
-// /**
-//  * Updates user email
-//  * @param newEmail new email address
-//  */
-// export const updateEmail = async (uid: string, newEmail: string) => {
-//   try {
-//     await setDoc(doc(db, userCollection), { email: newEmail }, { merge: true });
-//   } catch (e) {
-//     console.error("Error updating document: ", e);
-//     alert("Internal error updating email. Please try again later.");
-//   }
-// };
-
-// /**
-//  * Updates user profile picture
-//  * @param newProfilePictureUrl new profile picture URL
-//  */
-// export const updateProfilePicture = async (uid: string, newProfilePictureUrl: string) => {
-//   try {
-//     await setDoc(
-//       doc(db, userCollection),
-//       { profilePicture: newProfilePictureUrl },
-//       { merge: true }
-//     );
-//   } catch (e) {
-//     console.error("Error updating document: ", e);
-//     alert("Internal error updating profile picture. Please try again later.");
-//   }
-// };
-
-// /**
-//  * Adds preference to user's preferences
-//  * @param preference preference object
-//  */
-// export const addPreference = async (preference: Preference) => {
-//   try {
-//     const docRef = await addDoc(collection(db, preferenceCollection), {
-//       name: preference.name,
-//     });
-//   } catch (e) {
-//     console.error("Error adding document: ", e);
-//     alert("Internal error adding preference. Please try again later.");
-//   }
-// };
-
-// /**
-//  * Removes preference from user's preferences
-//  * @param preferenceId preference id
-//  */
-// export const removePreference = async (preferenceId: string) => {
-//   try {
-//     await deleteDoc(doc(db, preferenceCollection, preferenceId));
-//   } catch (e) {
-//     console.error("Error removing document: ", e);
-//     alert("Internal error removing preference. Please try again later.");
-//   }
-// };
-
-// /**
-//  * Fetches preferences from user
-//  */
-// export const fetchPreferences = async () => {
-//   try {
-//     const querySnapshot = await getDocs(collection(db, preferenceCollection));
-//     const preferences: Preference[] = [];
-//     querySnapshot.forEach((doc) => {
-//       preferences.push(doc.data().name);
-//     });
-//     return preferences;
-//   } catch (e) {
-//     console.error("Error getting documents: ", e);
-//     alert("Internal error fetching preferences. Please try again later.");
-//   }
-// };
