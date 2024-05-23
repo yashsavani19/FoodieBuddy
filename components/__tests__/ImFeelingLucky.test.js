4a7fb6ac1e45642516a24511385ddac3ea8f2677
Joshua-Frost <112672181+Joshua-Frost@users.noreply.github.com>
2024-05-23 12:17:43 +1200

    further progress on testing

--- components/__tests__/ImFeelingLucky.test.js
+++ components/__tests__/ImFeelingLucky.test.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import ImFeelingLucky from '../ImFeelingLucky';
+import ImFeelingLucky from '../ImFeelingLucky'; // Adjust the import according to your file structure
+import { AppContext } from '@/context/AppContext';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
 
-jest.mock("@firebase/firestore", () => {
-  return {
-    collection: jest.fn().mockReturnThis(),
-    doc: jest.fn().mockReturnThis(),
-    addDoc: jest.fn().mockResolvedValue(),
-    get: jest.fn().mockResolvedValue(),
-    onSnapshot: jest.fn(),
-  };
-});
+jest.mock('expo-router', () => ({
+  useNavigation: () => ({
+    navigate: jest.fn(),
+  }),
+}));
 
-test('it renders correctly', () => {
-  const { getByText } = render(<ImFeelingLucky />);
-  expect(getByText('Expected Text')).toBeTruthy();
+const mockContextValue = {
+  localRestaurants: [{ name: 'Restaurant 1' }, { name: 'Restaurant 2' }],
+};
+
+test('renders correctly', () => {
+  const { getByTestId } = render(
+    <AppContext.Provider value={mockContextValue}>
+      <NavigationContainer>
+        <ImFeelingLucky>
+          <Text>I'm Feeling Lucky</Text>
+        </ImFeelingLucky>
+      </NavigationContainer>
+    </AppContext.Provider>
+  );
+  expect(getByTestId('lucky-button')).toBeTruthy();
 });
