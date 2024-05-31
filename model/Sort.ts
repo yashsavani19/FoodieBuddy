8fd334b9524df3b6ba436f628603f23d791b54b4
yashsavani19 <yashsavani19@gmail.com>
2024-05-31 13:33:52 +1200

    Created Sort Models

---
+++ model/Sort.ts
@@ -0,0 +1,4 @@
+export interface Sort {
+    sort?: string;
+    selected?: boolean;
+}
---
+++ model/SortOptions.ts
@@ -0,0 +1,32 @@
+import {Sort} from './Sort';
+
+export const SortOptions: Sort[] = [
+    {
+        sort: "Preference", selected: false
+    },
+    {
+    sort: "Price: Low to High", selected: false
+    },
+    {
+    sort: "Price: High to Low", selected: false
+    },
+    {
+    sort: "Rating: High to Low", selected: false
+    },
+    {
+    sort: "Rating: Low to High", selected: false
+    },
+    {
+    sort: "A-Z", selected: false
+    },
+    {
+    sort: "Z-A", selected: false
+    },
+    {
+    sort: "Distance (Nearest)", selected: false
+    },
+    {
+    sort: "Distance (Farthest)", selected: false
+    },
+    
+    ]
