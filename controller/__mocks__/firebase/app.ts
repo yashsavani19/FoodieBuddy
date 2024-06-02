8c3ad45e58101b173ed1e8d953ee7bbf150c85f3
Joshua-Frost <gky3846@autuni.ac.nz>
2024-06-03 11:13:07 +1200

    Revert "Merge branch"
    Making Jest work breaks the app
    
    This reverts commit 732948e39096bd2ef6e4ec1c9002f2d86c74ba46, reversing
    changes made to 0b4b15b29ad0421e3c8a4e30881a3ea77282311a.

--- controller/__mocks__/firebase/app.ts
+++
@@ -1,6 +0,0 @@
-const mockFirebaseApp = {
-    initializeApp: jest.fn(),
-    firestore: jest.fn(),
-  };
-  
-  export default mockFirebaseApp;
--- jest.setup.js
+++
@@ -1,2 +0,0 @@
-jest.mock('firebase/app', () => require('./controller/__mocks__/firebase/app'));
-jest.mock('firebase/firestore', () => require('./controller/__mocks__/firebase/firestore'));
