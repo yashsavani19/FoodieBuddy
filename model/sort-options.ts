1c9fe0ae42fdafb101c20547c5546c6c1f19de02
yashsavani19 <yashsavani19@gmail.com>
2024-06-04 08:22:59 +1200

    Deleted unneccesary file

--- model/sort-options.ts
+++
@@ -1,8 +0,0 @@
-import { Sort } from "@/model/Sort";
-
-export const sortOptions: Sort[] = [
-  { sortOption: "All", selected: true },
-  { sortOption: "Popular", selected: false },
-  { sortOption: "New", selected: false },
-  { sortOption: "Top Rated", selected: false },
-];
