11b5e47af6268983877fa35e9140b64d654dd767
Joshua-Frost <gky3846@autuni.ac.nz>
2024-06-04 01:34:53 +1200

    Update readme

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/yashsavani19/FoodieBuddy.git
+
+# Navigate to the project directory
+cd FoodieBuddy || exit
+
+# Install Dependencies
+npm install
+
+# Start server
+npm start
