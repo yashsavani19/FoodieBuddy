b79b3916408b07aeafc9e414bb02d1e0822d1484
Param Patel <parampatel892002@gmail.com>
2024-06-04 16:04:10 +1200

    fixed path and installed babel for testing

--- components/__tests__/StartupGuide.test.js
+++ components/__tests__/StartupGuide.test.js
@@ -1,7 +1,7 @@
 // StartupGuide.test.js
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
-import StartupGuide from '@components/modals/StartupGuide.tsx';
+import StartupGuide from '@/components/modals/StartupGuide.tsx';
 
 describe('StartupGuide', () => {
   it('calls onClose when the close button is pressed', () => {
